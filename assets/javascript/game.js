2f3fe8443fe5878273b295e7cf8e873d173b0ef0
hatab002 <hatab002@umn.edu>
2018-03-05 15:26:14 -0600

    Final version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,30 @@
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var wordBank = ["friends", "suits", "lost", "seinfield", "homeland", "supernatural", "rome", "limitless", "scrubs"];
-var img = ["assets/images/hangman2.png", "assets/images/hangman3.png", "assets/images/hangman4.png", "assets/images/hangman5.png", "assets/images/hangman6.png", "assets/images/hangman7.png"]
-var remainingGuesses = 6;
-// var wins = 0;
+var wins = 0;
 var losses = 0;
-var blanks = [];
-var keyPressed = [];
-var wrongGuess = [];
-// while ( remainingGuesses > 0){
-var chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-// var splitWord = chosenWord.split('')
-    function startGame () {
-        var backgroundMusic = document.createElement("audio");
-        backgroundMusic.setAttribute("src", "assets/background-music.mp3");
-        backgroundMusic.play();
-        
-    
+function startGame () {
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var wordBank = ["friends", "suits", "lost", "seinfield", "homeland", "supernatural", "rome", "limitless", "scrubs"];
+    var img = ["assets/images/hangman1.png", "assets/images/hangman2.png", "assets/images/hangman3.png", "assets/images/hangman4.png", "assets/images/hangman5.png", "assets/images/hangman6.png", "assets/images/hangman7.png"]
+    var chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    var remainingGuesses = 6;
+    var blanks = [];
+    var keyPressed = [];
+    var wrongGuess = [];
+
+// Background Music
+
+    var backgroundMusic = document.createElement("audio");
+    backgroundMusic.setAttribute("src", "assets/background-music.mp3");
+    backgroundMusic.play();
+
+// Setup
+
+    document.getElementById("lettersChosen").innerHTML = [];
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("img").src = "assets/images/hangman1.png"
+
+// Adding Blanks
+
     for(i = 0; i < chosenWord.length; i++){
         blanks.push(' _ ');
     }
@@ -24,183 +33,109 @@ var chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 
     document.getElementById("currentWord").innerHTML = blanks.join(" ");
 
-    var wrongGuess = [];
-
+// When player presses key
     
-    var remainingGuesses = 6;
-    document.getElementById("guesses").innerHTML = remainingGuesses;
-    // var wins = 0;
-    // document.getElementById("wins").innerHTML = wins;
     document.onkeypress = function (event){
         keyPressed = event.key;
         if (alphabet.indexOf(keyPressed) === -1){
             alert("Please enter a valid letter!")
         } else if (alphabet.indexOf(keyPressed) > -1){
-            // var isGameDone = false;
+            
             var isLetterFound = false;
             for ( j = 0; j < chosenWord.length; j++){
                 if(splitWord[j] === keyPressed){
                     blanks[j] = keyPressed;
                     document.getElementById("currentWord").innerHTML = blanks.join("");
                     isLetterFound = true;
-                    
+
                     console.log(blanks);
-                    
-                } 
+
+                }
 
                 }
             if ( isLetterFound === false) {
-                
+                    
+// Sound effect for wrong guess
+                var audioElement = document.createElement("audio");
+                audioElement.setAttribute("src", "assets/pain.mp3");
+                audioElement.play();
+
                 if (document.getElementById("lettersChosen").innerHTML.indexOf(keyPressed) === -1){
                     document.getElementById("lettersChosen").innerHTML = document.getElementById("lettersChosen").innerHTML + " " + keyPressed;
-                    
+
                     remainingGuesses--;
                     document.getElementById("guesses").innerHTML = remainingGuesses;
 
                     }
-    // changing picture
-    var audioElement = document.createElement("audio");
-      audioElement.setAttribute("src", "assets/pain.mp3")
+// changing picture
+    
                     if(remainingGuesses === 5){
                         document.getElementById("img").src = "assets/images/hangman2.png";
-                        audioElement.play();
-                    } 
+                        
+                    }
                     if (remainingGuesses === 4){
                         document.getElementById("img").src = "assets/images/hangman3.png";
-                        audioElement.play();
+                        
                     }
                     if (remainingGuesses === 3){
                         document.getElementById("img").src = "assets/images/hangman4.png";
-                        audioElement.play();
+                        
                     }
                     if (remainingGuesses === 2){
                         document.getElementById("img").src = "assets/images/hangman5.png";
-                        audioElement.play();
+                        
                     }
                     if (remainingGuesses === 1){
                         document.getElementById("img").src = "assets/images/hangman6.png";
-                        audioElement.play();
+                        
                     }
                     if (remainingGuesses === 0){
                         document.getElementById("img").src = "assets/images/hangman7.png";
-                        audioElement.play();
-                    }
-                    
-                }
-            } 
-            // end boolean 
-            
-            if(blanks.indexOf(' _ ') === -1) {
-                        alert("YOU WIN!!!");
-                        alert("Word is " + chosenWord.toUpperCase());
                         
                     }
-            if(remainingGuesses < 0){
-                alert("Game Over!");
-                location.reload();
+
+                }
             }
-            
+// Win/Lose scenario 
+
+            if(blanks.indexOf(' _ ') === -1) {
+
+                        alert("YOU WIN!!!");
+                        alert("Word is " + chosenWord.toUpperCase());
+                        wins++;
+                        document.getElementById("wins").innerHTML = wins;
+                        startGame();
+                    }
+            if(remainingGuesses === 0){
+                alert("Game Over!");
+                alert("Word was " + chosenWord.toUpperCase());
+                losses++;
+                document.getElementById("losses").innerHTML = losses;
+                startGame();
+            }
+
         }
     };
 
-        var button = document.getElementById("button");
-        button.addEventListener("click", function buttonFunction(){
-            startGame();
-            if(remainingGuesses <= 6){
-                    document.getElementById("button").setAttribute("disabled", true);    
-                
-            }
-            
-        })
 
-        var button1 = document.getElementById("button1");
-        button1.addEventListener("click", function buttonFunction1(){
-            location.reload();
-        
-        })
-        
-    
+// Buttons
 
-        // if ( chosenWord.indexOf(keyPressed) > -1)
-        // { console.log(keyPressed);
-        //     for (var j = 0; j < chosenWord.length; j++) {
-        //             if (chosenWord[j] === keyPressed) {
-        //             blanks[j] = keyPressed;
-        //             remainingGuesses--;}}
-        
-        // }
+var button = document.getElementById("button");
+    button.addEventListener("click", function buttonFunction(){
+        startGame();
+        if(remainingGuesses <= 6){
+            document.getElementById("button").setAttribute("disabled", true);
 
-        //     else {
-        //         // document.getElementById("lettersChosen").innerHTML = wrongGuess;
-        //         // console.log(wrongGuess);
-        //         var wrongGuess = document.createElement("p");
-        //         // function wrong (){ wrongGuess.push(keyPressed)};
-        //         wrongGuess.innerHTML = keyPressed;
-        //         document.getElementById("lettersChosen").appendChild(wrongGuess);
-        //         // remainingGuesses--;
-        //         // console.log(remainingGuesses)
-        //         }
-        //     // }
-        
-    // }
-        
-        
-        
+        }
 
+    })
 
-// var answer = [];
-// for(i = 0; i < chosenWord.length; i++){
-//     answer[i] = "-";
-// }
-// var remainingCharacters = chosenWord.length;
+var button1 = document.getElementById("button1");
+    button1.addEventListener("click", function buttonFunction1(){
+        location.reload();
 
-// // while (remainingCharacters > 0) {
+    })
 
-//     document.onkeyup = function(event) {
-//     var keyPressed = event.key;
-//     console.log(keyPressed);
-//         var updatedLetters = document.createElement("div");
-//         updatedLetters.innerHTML = keyPressed;
-//         document.getElementById("lettersChosen").appendChild(updatedLetters);
-
-// }
-    // Show the player their progress
-
-//     alert(answer.join(" "));
-//     // Get a guess from the player
-//     var guess = prompt("Guess a letter, or click Cancel to stop playing.");
-//     if (guess === null) {
-//     // Exit the game loop
-//     break;
-//     } else if (guess.length !== 1) {
-//     alert("Please enter a single letter.");
-//     } else {
-//     // Update the game state with the guess
-//     
-//     }
-// }
-// }
-    
-    
-// while(remainingCharacters = chosenWord.length){
-// var guess = document.createElement('div');
-// guess.innerHTML = "remainingGuesses";
-// document.getElementById("guesses").appendChild(guess);
-
-
-
-// }
-//     if (keyPressed === "" )
-
-    
-
-
-//     var blanks = document.createElement("div");
-//     blanks.innerHTML = answer[i];
-//     document.getElementById("currentWord").appendChild(blanks);
-// };
-
-
-// }
+ 
 
 
